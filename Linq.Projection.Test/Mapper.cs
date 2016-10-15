15c25a903d5779f617d4d8b845745676a1eae9be
Fabricio Dupont <fabriciodupont@hotmail.com>
2016-10-15 20:08:06 -0300

    Initial Tests

--- Linq.Projection.Test/Mapper.cs
+++ Linq.Projection.Test/Mapper.cs
@@ -8,13 +8,49 @@
     [TestClass]
     public class Mapper
     {
-        [TestMethod]
-        public void Custom_map_properties()
+        public IList<Pessoa> ObterPessoas()
         {
-            var pessoas = new List<Pessoa>()
+            IList<Pessoa> pessoas = new List<Pessoa>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                pessoas.Add(new Pessoa() { Id = i, Nome = "Pessoa" + i, Idade = i });
+            }
+
+            return pessoas;
+        }
+
+        [TestMethod]
+        public void Automatic_map_properties()
+        {
+            var pessoas = ObterPessoas()
                 .AsQueryable()
                 .Project<Pessoa>()
-                .To<Pessoa>(mapper => mapper.Map(t => t.Id, s => s.Id));
+                .To<DTODePessoa>();
+
+            Assert.IsInstanceOfType(pessoas, typeof(IQueryable<DTODePessoa>));
+        }
+
+        [TestMethod]
+        public void Ignore_property()
+        {
+            var pessoas = ObterPessoas()
+                .AsQueryable()
+                .Project<Pessoa>()
+                .To<DTODePessoa>(mapper => mapper.Ignore(a => a.Nome));
+
+            Assert.IsNull(pessoas.First().Nome);
+        }
+
+        [TestMethod]
+        public void Map_property()
+        {
+            var pessoas = ObterPessoas()
+                .AsQueryable()
+                .Project<Pessoa>()
+                .To<DTODePessoa>(mapper => mapper.Map(a => a.Nome, b => b.Nome + "Teste"));
+
+            Assert.AreEqual(pessoas.First().Nome, "Pessoa0Teste");
         }
     }
 
@@ -25,6 +61,11 @@
         public string Nome { get; set; }
 
         public int Idade { get; set; }
+    }
+
+    public class DTODePessoa
+    {
+        public string Nome { get; set; }
     }
 }
 
